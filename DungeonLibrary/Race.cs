46268ad0209925c2b361f667c186c3ecda41e789
Jace Cockrell <jace.c.IT@gmail.com>
2023-04-26 14:46:11 -0500

    created race and weapontype enums and added weapontype to wepon.cs

---
+++ DungeonLibrary/Race.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    //make it public
+    //make at least 5 races to the mix
+    //the intent is for the player object to have a property of this type.
+    //monsters will not have this.
+
+    public enum Race
+    {
+        Olympians,
+        Giants,
+        Titans,
+        Valkyries,
+        HelWalkers
+
+    }
+}
---
+++ DungeonLibrary/WeaponTypeEnum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    //Weapon will includ WeaponType, so make it public and make it enum.
+    //at least 5 types of weapons
+    // intent of this is to provide potential expansions, such as 
+    public enum WeaponTypeEnum
+    {
+        Leviathan_Axe,
+        Blades_of_Chaos,
+        Guardian_Shield,
+        Valkyrie_Armor,
+        Atreus_Bow,
+    }
+}
